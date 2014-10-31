ad9d6d0fd5521783e8379e35f4b6e0f88cf49d11
jjung <jjung@vailsys.com>
2014-10-31 11:15:32 -0500

    Adding parent and subtasks and writing unit tests to verify task completion progress.

--- src/Progression.js
+++ src/Progression.js
@@ -5,36 +5,88 @@ var EventDispatcher = JClass.extend(require('events').EventEmitter.prototype),
 
 var Progression = EventDispatcher.extend({
   init: function () {
-    this._tasks = new HashArray(['id']);
+    this._tasks = new HashArray(['id', 'group']);
+    this.lastProgress = 0;
   },
   getTasks: function () {
     return this._tasks;
   },
+  getTask: function (id) {
+    return this._tasks.get(id);
+  },
+  getChildrenFor: function (id) {
+    return this._tasks.getAsArray('$' + id);
+  },
   getProgress: function () {
     var total = this._tasks.sum('*', 'count', 'weight'),
       actual = this._tasks.filter('*', 'completed').sum('*', 'count', 'weight');
-    
+
     // Should be a value between 0 and 1
     return actual / total;
   },
-  addTask: function (task) {
+  addTask: function (task, groupId) {
+    var self = this;
+    
     task = typeof(task) == 'string' ? {id: task} : task;
+
+    if (this._tasks.has(task.id))
+      throw Error('Cannot add the same task twice '. task);
+
     task.count = task.count || 1;
     task.weight = task.weight || 1;
     task.countCompleted = task.countCompleted || 0;
     task.completed = task.countCompleted || false;
+    task.group = '$' + groupId;
+  
+    task.isParent = function () {
+      return self.getChildrenFor(this.id).length != 0;
+    }
 
     this._tasks.add(task);
 
     this.emit('progress', this.getProgress());
   },
-  completeTask: function (id) {
+  update: function () {
+    if (this.getProgress() != this.lastProgress)
+    {
+      this.emit('progress', this.getProgress());
+
+      if (this.getProgress() == 1.0)
+        this.emit('finished');
+    }
+  },
+  progress: function (id) {
     var task = this._tasks.get(id);
+
+    // Verify children tasks are completed.
+    var children = this._tasks.getAsArray('$' + id);
+    if (children.length)
+    {
+      var childrenCompleted = true;
+      children.forEach(function (child) {
+        childrenCompleted = childrenCompleted && child.completed;
+      });
+
+      if (!childrenCompleted)
+        throw Error('Cannot complete parent task until children in its group are completed');
+    }
 
     task.countCompleted += Math.min(task.count - task.countCompleted, 1);
     task.completed = task.countCompleted == task.count;
 
     this.emit('progress', this.getProgress());
+
+    if (this.getProgress() > 1.0)
+      throw Error('Something bad happened, progress is greater than 1.0 and this should never happen ' +this.getProgress());
+
+    // Needed for the update() function.
+    this.lastProgress == this.getProgress();
+
+    if (task.completed)
+      this.emit('completed', task);
+
+    if (this.getProgress() == 1.0)
+      this.emit('finished');
   }
 });
 
--- test/progressionTest.js
+++ test/progressionTest.js
@@ -10,13 +10,107 @@ describe('Progression', function() {
     });
 	});
 
-	describe('addTask(...) should work', function() {
+	describe('addTask(...)', function() {
 		var p = new Progression();
 
     p.addTask('task1');
 
-    it('should have a progression task array', function() {
+    it('after addTask, should have a progression task array', function() {
       assert.equal(p.getTasks().all.length, 1);
+    });
+
+    it('should fail if the same task is added twice', function() {
+      assert.throws(function () {p.addTask('task1');}, function (err) {return true;});
+    });
+	});
+  
+	describe('getProgress() and progress()', function() {
+		var p = new Progression();
+
+    p.addTask('task1');
+    p.addTask('task2');
+    p.addTask('task3');
+    p.addTask('task4');
+    p.addTask('task5');
+    p.addTask('task6');
+
+    it('should have 0 percentage complete after adding no complete items', function() {
+      assert.equal(p.getProgress(), 0);
+    });
+
+    it('should have 1/n percentage complete after completing each item', function() {
+      for (var i = 1; i <= 6; i++)
+      {
+        p.progress('task' + i);
+        assert.equal(p.getProgress(), i / 6);
+      }
+    });
+
+    it('should have n/(n+1) percentage complete after completing each item AND adding another', function() {
+      p.addTask('taskFinal')
+      assert.equal(p.getProgress(), 6/7);
+    });
+	});
+  
+	describe('getProgress() and progress() with weighted tasks and no counts', function() {
+		var p = new Progression();
+
+    p.addTask('main1');
+    p.addTask({id: 'sub11', weight: 0.1}, 'main1');
+    p.addTask({id: 'sub12', weight: 0.1}, 'main1');
+    p.addTask({id: 'sub13', weight: 0.1}, 'main1');
+
+    p.addTask('main2');
+    p.addTask({id: 'sub21', weight: 0.1}, 'main2');
+    p.addTask({id: 'sub22', weight: 0.1}, 'main2');
+    p.addTask({id: 'sub23', weight: 0.1}, 'main2');
+    
+    p.addTask('main3');
+    p.addTask({id: 'sub31', weight: 0.2}, 'main3');
+    p.addTask({id: 'sub32', weight: 0.2}, 'main3');
+    p.addTask({id: 'sub33', weight: 0.2}, 'main3');
+
+    var weightTotal = (3 * 1) + (0.1 * 3) + (0.1 * 3) + (0.2 * 3);
+
+    it('should have 0 percentage complete after adding no complete items', function() {
+      assert.equal(p.getProgress(), 0);
+    });
+
+    it('should error if you try to complete a parent task before children finish', function() {
+      assert.equal(p.getProgress(), 0);
+    });
+
+    it('should have appropriate weighted percentage complete after completing each item', function() {
+      var all = p.getTasks().all,
+        curWeightCompleted = 0;
+
+      // Complete all subtasks
+      all.forEach(function (task) {
+        if (!task.isParent())
+        {
+          p.progress(task.id);
+          
+          assert.equal(task.countCompleted, 1, 'task.completedCount should be 1!');
+          assert.equal(task.completed, true,'task.completed should be true!');
+          
+          curWeightCompleted += task.weight;
+          assert.equal(Math.round(p.getProgress() * 100), Math.round((curWeightCompleted / weightTotal) * 100));
+        }
+      });
+
+      // Complete parent tasks
+      all.forEach(function (task) {
+        if (task.isParent())
+        {
+          p.progress(task.id);
+          
+          assert.equal(task.countCompleted, 1, 'task.completedCount should be 1!');
+          assert.equal(task.completed, true,'task.completed should be true!');
+          
+          curWeightCompleted += task.weight;
+          assert.equal(Math.round(p.getProgress() * 100), Math.round((curWeightCompleted / weightTotal) * 100));
+        }
+      });
     });
 	});
 });
